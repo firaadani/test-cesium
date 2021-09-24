b667ed046d9f80a47ab93f392f56a3e981c9338e
firaadani <88301116+firaadani@users.noreply.github.com>
2021-09-24 15:24:05 +0700

    add layering show & hide

--- src/App.js
+++ src/App.js
@@ -1,72 +1,14 @@
 import React from "react";
-import {
-  Cartesian3,
-  ArcGisMapServerImageryProvider,
-  Math as CesiumMath,
-} from "cesium";
-import {
-  KmlDataSource,
-  Viewer,
-  Entity,
-  ImageryLayer,
-  Camera,
-  CameraFlyTo,
-} from "cesium-react";
+
+import signalIcon from "./signal-tower.svg";
+
+import Resium from "./Resium";
 
 function App() {
   return (
-    <Viewer full>
-      <KmlDataSource
-        // data={"./kml/KML_Samples.kml"}
-        // data={"./kml/UNet_Generated_MUL053MM1_PUNCAKMULIA.kmz"}
-        url={
-          "https://naufalibnusalam.com/kml/UNet_Generated_MUL053MM1_PUNCAKMULIA.kmz"
-        }
-        // url={"https://naufalibnusalam.com/kml/slg0070My%20Places.kml"}
-        onError={(err) => console.log(err)}
-        onLoad={(data) => console.log(data)}
-        show
-        clustering
-        name="test"
-      />
-      <Entity
-        name="tokyo"
-        position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-        point={{ pixelSize: 10 }}
-      >
-        test
-      </Entity>
-      {/* <ImageryLayer
-        imageryProvider={
-          new ArcGisMapServerImageryProvider({
-            url: "//services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer",
-          })
-        }
-      /> */}
-      {/* camera will be moved instantly. */}
-      {/* you can use for initializing camera position. */}
-      {/* <Camera
-        view={{
-          destination: Cartesian3.fromDegrees(
-            -122.0839597145766,
-            37.42222904525232,
-            2000
-          ),
-          // orientation: { pitch: CesiumMath.toRadian(-60) },
-        }}
-      /> */}
-
-      {/* camera animation */}
-      {/* <CameraFlyTo
-        destination={Cartesian3.fromDegrees(
-          -122.0839597145766,
-          37.42222904525232,
-          500
-        )}
-        // orientation={{ pitch: CesiumMath.toRadian(-60) }}
-        duration={3}
-      /> */}
-    </Viewer>
+    <>
+      <Resium />
+    </>
   );
 }
 
---
+++ src/Resium.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import {
+  Cartesian3,
+  ArcGisMapServerImageryProvider,
+  Math as CesiumMath,
+  BoundingSphere,
+  HeadingPitchRange,
+} from "cesium";
+import {
+  Viewer,
+  Entity,
+  EntityDescription,
+  PointGraphics,
+  CameraFlyToBoundingSphere,
+  KmlDataSource,
+  BillboardGraphics,
+} from "resium";
+import Color from "cesium/Source/Core/Color";
+
+import signalIcon from "./antenna.svg";
+import buildingIcon from "./building.svg";
+
+const Resium = () => {
+  const [showPoints, setShowPoints] = useState(false);
+  const [showBuildings, setShowBuildings] = useState(false);
+  const points = [
+    {
+      lat: 137.96889541,
+      long: -3.70455916,
+    },
+    {
+      lat: 137.98929621,
+      long: -3.72710416,
+    },
+    {
+      lat: 137.96769999,
+      long: -3.73705542,
+    },
+    {
+      lat: 137.97085574,
+      long: -3.71154126,
+    },
+  ];
+
+  const buildings = [
+    {
+      lat: 137.96282225,
+      long: -3.71896487,
+    },
+    {
+      lat: 138.08469435,
+      long: -3.68404432,
+    },
+    {
+      lat: 138.10768344,
+      long: -3.71908523,
+    },
+    {
+      lat: 138.10779001,
+      long: -3.74408637,
+    },
+  ];
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          zIndex: "100",
+          backgroundColor: "white",
+          width: "10%",
+          height: "5%",
+          borderRadius: "10px",
+          marginTop: "20px",
+          marginLeft: "20px",
+          padding: "10px",
+        }}
+      >
+        <label for="points">
+          <input
+            id="points"
+            type="checkbox"
+            onClick={() => setShowPoints((prev) => !prev)}
+          />
+          BTS
+        </label>
+        <label for="buildings">
+          <input
+            id="buildings"
+            type="checkbox"
+            onClick={() => setShowBuildings((prev) => !prev)}
+          />
+          Buildings
+        </label>
+      </div>
+      <Viewer full>
+        {showPoints &&
+          points.map((e, id) => {
+            return (
+              <Entity
+                name={`BTS ${id}`}
+                position={Cartesian3.fromDegrees(e.lat, e.long, 100)}
+              >
+                {/* <PointGraphics pixelSize={100} color={Color.YELLOW} /> */}
+                {/* <EntityDescription>
+            <h1>Hello world</h1>
+            <p>test</p>
+          </EntityDescription> */}
+                <BillboardGraphics image={signalIcon} height={30} width={30} />
+              </Entity>
+            );
+          })}
+        {showBuildings &&
+          buildings.map((e, id) => {
+            return (
+              <Entity
+                name={`Building ${id}`}
+                position={Cartesian3.fromDegrees(e.lat, e.long, 100)}
+              >
+                {/* <PointGraphics pixelSize={100} color={Color.YELLOW} /> */}
+                {/* <EntityDescription>
+            <h1>Hello world</h1>
+            <p>test</p>
+          </EntityDescription> */}
+                <BillboardGraphics
+                  image={buildingIcon}
+                  height={30}
+                  width={30}
+                />
+              </Entity>
+            );
+          })}
+
+        <CameraFlyToBoundingSphere
+          boundingSphere={
+            new BoundingSphere(
+              Cartesian3.fromDegrees(137.96889541, -3.70455916, 200),
+              0
+            )
+          }
+          offset={new HeadingPitchRange(0, CesiumMath.toRadians(-30), 80000)}
+          duration={3}
+        />
+        <KmlDataSource
+          data={
+            "https://naufalibnusalam.com/kml/UNet_Generated_MUL053MM1_PUNCAKMULIA.kmz"
+          }
+          onLoad={(dataSource) => {
+            // you can modify data source here
+            dataSource.entities.values.forEach((e) => {
+              e.label = undefined;
+            });
+          }}
+        />
+      </Viewer>
+    </div>
+  );
+};
+
+export default Resium;
